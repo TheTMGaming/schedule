1382a9cea79edd1f626b8fbdb47b92399334c621
Yura Perov <iury.perov@gmail.com>
2021-11-28 14:21:24 +0500

    Added task.html

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php require_once "src/task.php"; ?>
+</body>
+</html>
---
+++ src/task.php
@@ -0,0 +1,21 @@
+
+<div class="task">
+    <div class="date">
+        <div class="day">
+            <span class="day-text">05</span>
+        </div>
+        <div class="month-time">
+            <span class="month-text">August</span>
+            <span class="time-text">11:00-14:00</span>
+        </div>
+    </div>
+
+    <div class="info">
+        <span class="title-text">Formation of the organizational structure of the company in the face of uncertainty.</span>
+        <div class="description">
+            <span class="description-text">Online master-class</span>
+        </div>
+    </div>
+
+    <button class="button-view">View more</button>
+</div>
